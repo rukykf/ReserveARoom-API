0409bbb62642a550c5bb6349365b0237d3ccde15
Kofi Oghenerukevwe <rukykf@gmail.com>
2020-04-30 03:51:30 +0100

    setup server for front-end development

--- server/index.js
+++ server/index.js
@@ -5,20 +5,31 @@ const express = require("express")
 const session = require("express-session")
 const history = require("connect-history-api-fallback")
 const path = require("path")
-const app = express()
-const port = process.env.PORT
+const cors = require("cors")
+
 const ReservationsController = require("./src/controllers/ReservationsController")
 const AuthenticationController = require("./src/controllers/AuthenticationController")
+const routes = require("./src/routes")
+
+const app = express()
+const port = process.env.PORT
+
+if (process.env.NODE_ENV === "development") {
+  // enable cors to make it easy to make api calls to the server from my spa vue project
+  app.use(cors())
+} else {
+  // ensure that all direct get requests that accept text/html are redirected to index.html
+  app.use(history())
+}
 
 app.use(express.static(path.join(__dirname, "static/uploads")))
 app.use(express.static(path.join(__dirname, "static/dist")))
 
 app.use(express.json())
-app.use(session({ secret: "hello-world", resave: false, saveUninitialized: true }))
+app.use(session({ secret: process.env.APP_SECRET, resave: false, saveUninitialized: true }))
 
-// app.use(history())
+app.use("/api", routes)
+app.post("/api/login", AuthenticationController.login)
+app.post("/api/logout", AuthenticationController.logout)
 
-app.post("/rooms/:id", ReservationsController.createReservationForRoom)
-app.post("/logout", AuthenticationController.logout)
-
-app.listen(port, "0.0.0.0", () => console.log(`ReserveARoom app listening on port ${port}!`))
+app.listen(port, process.env.APP_HOST, () => console.log(`ReserveARoom app listening on port ${port}!`))
---
+++ server/src/routes.js
@@ -0,0 +1,8 @@
+const { Router } = require("express")
+// const UsersController = require("../controllers/configuration/UsersController")
+
+let router = Router()
+
+// router.get("/users", UsersController.index)
+
+module.exports = router
