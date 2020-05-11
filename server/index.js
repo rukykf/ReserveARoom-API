2702e5ba8ade60cf6ce55eadf986c88dcbcaaaa1
Kofi Oghenerukevwe <rukykf@gmail.com>
2020-05-11 17:25:46 +0100

    registered a few routes

--- server/index.js
+++ server/index.js
@@ -7,29 +7,25 @@ const history = require("connect-history-api-fallback")
 const path = require("path")
 const cors = require("cors")
 
-const ReservationsController = require("./src/controllers/ReservationsController")
-const AuthenticationController = require("./src/controllers/AuthenticationController")
 const routes = require("./src/routes")
 
 const app = express()
 const port = process.env.PORT
 
+
+app.use(express.static(path.join(__dirname, "static/uploads")))
+app.use(express.static(path.join(__dirname, "static/dist")))
+app.use(express.json())
+
 if (process.env.NODE_ENV === "development") {
-  // enable cors to make it easy to make api calls to the server from my spa vue project
+  // enable cors to make it easy to make api calls to the server from my vue spa project
   app.use(cors())
 } else {
   // ensure that all direct get requests that accept text/html are redirected to index.html
   app.use(history())
 }
 
-app.use(express.static(path.join(__dirname, "static/uploads")))
-app.use(express.static(path.join(__dirname, "static/dist")))
-
-app.use(express.json())
-app.use(session({ secret: process.env.APP_SECRET, resave: false, saveUninitialized: true }))
 
 app.use("/api", routes)
-app.post("/api/login", AuthenticationController.login)
-app.post("/api/logout", AuthenticationController.logout)
-
+app.use(session({ secret: process.env.APP_SECRET, resave: false, saveUninitialized: true }))
 app.listen(port, process.env.APP_HOST, () => console.log(`ReserveARoom app listening on port ${port}!`))
--- server/src/routes.js
+++ server/src/routes.js
@@ -1,9 +1,20 @@
 const { Router } = require("express")
 const ReservationsController = require("../src/controllers/ReservationsController")
 const RoomTypesController = require("../src/controllers/RoomTypesController")
+const AuthenticationController = require("../src/controllers/AuthenticationController")
 
 let router = Router()
 
-router.get("/room-types", RoomTypesController.index)
+router.post("/session-login", AuthenticationController.login)
+router.post("/session-logout", AuthenticationController.logout)
+
+router.post("/rooms/:id/reservations", ReservationsController.createReservationForRoom)
+router.get("/rooms/:id/reservations", ReservationsController.getReservationsCalendarForRoom)
+
+router.get("/reservations", ReservationsController.getAllReservations)
+router.post("/reservations/:id", ReservationsController.updateReservationStatus)
+router.put("/reservations/:id/otp", ReservationsController.resendOtpForReservation)
+router.post("/reservations/:id/otp", ReservationsController.confirmReservation)
+
 
 module.exports = router
